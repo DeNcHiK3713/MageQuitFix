3eee6b2b8c8629d290db236ff528f005f1f8df23
Denis Suchok <den6281505@gmail.com>
2023-08-11 01:24:50 +0300

    Updated nuget package.

--- MageQuitFix/Plugin.cs
+++ MageQuitFix/Plugin.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 namespace MageQuitFix
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
 
@@ -16,7 +16,7 @@ namespace MageQuitFix
             MouseSensitivityEntry = Config.Bind("Input", "MouseSensitivity", 0.05f, new ConfigDescription("Mouse sensitivity", new AcceptableValueRange<float>(0.001f, 1f)));
             MouseSensitivityEntry.SettingChanged += (_, _) => Globals.wizard_cursor?.SetSensitivity(MouseSensitivityEntry.Value);
             var harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
 }
